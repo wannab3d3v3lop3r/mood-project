485385fe9c4fe74b1252dfb662141efe37eee50c
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-03-04 21:55:09 -0500

    install dependencies and added gitignore

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+/*      line activates static asset sharing and allows us to server HTML, CSS, image, etc. 
+files from a public folder hosted on the same server as our app.        */
+
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);

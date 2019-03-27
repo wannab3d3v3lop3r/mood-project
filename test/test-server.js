563654502939293823d01fe814de81b7ccb4066a
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-03-27 04:04:34 -0400

    updated path on test-server file for travis-cl

--- test/test-server.js
+++ test/test-server.js
@@ -2,9 +2,11 @@
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const faker = require('faker');
+
+const {TEST_DATABASE_URL} = require('../config');
 const {app, runServer, closeServer} = require("../server");
-const config = require('../config');
-const {Journal} = require('../journalModel');
+const {Journal} = require('../journal/model');
 
 const expect = chai.expect;
 
@@ -12,44 +14,20 @@ chai.use(chaiHttp);
 
 describe("Testing", function() {
 
-  beforeEach(function(){
-    return runServer(config.TEST_DATABASE_URL);
-  })
-
-  beforeEach(function(){
-    //thinkful way
-    return Journal.insertMany([{
-      mood: "happy",
-      title: "Title",
-      thoughts: "thoughts",
-      publishDate: Date.now()
-    }])
+  before(function(){
+    return runServer(TEST_DATABASE_URL);
   });
 
-  afterEach(function(){
+  after (function (){
     return closeServer();
-  })
-
-  // it("Recieve a 200 status code", function() {
-  //   return chai
-  //     .request(app)
-  //     .get("/")
-  //     .then(function(res) {
-  //       expect(res).to.have.status(200);
-  //     });
-  // });
+  });
 
   it("Getting /journal-post responds with a journal and getting a status of 200", function() {
     return chai
       .request(app)
-      .get("/journal-post")
+      .get("/api/journal-post")
       .then(function(res) {
         expect(res).to.have.status(200);
-        expect(res.body).to.eql([{      
-          mood: "happy",
-          title: "Title",
-          thoughts: "thoughts"
-        }]);
-      });
-  });
+      })
+  })
 });
